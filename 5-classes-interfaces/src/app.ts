744fe1b06da02a4f5f123fceab53b5cee50924bb
Aaron Nguyen <me@aaronnguyen.dev>
2022-03-06 18:53:57 -0500

    62

--- 5-classes-interfaces/src/app.ts
+++ 5-classes-interfaces/src/app.ts
@@ -1,1 +1,34 @@
-class Department {}
+class Department {
+  #name: string;
+  #employees: string[] = [];
+
+  constructor(name: string) {
+    this.#name = name;
+  }
+  get name() {
+    return this.#name;
+  }
+  set name(value: string) {
+    this.#name = value;
+  }
+
+  describe(this: Department) {
+    console.log(`Department ${this.name}`);
+  }
+
+  add(employee: string) {
+    this.#employees.push(employee);
+  }
+  get employees() {
+    return this.#employees;
+  }
+  set employees(value: string[]) {
+    this.#employees = value;
+  }
+}
+
+const accounting = new Department("Accounting");
+console.log(accounting);
+accounting.add("Max");
+accounting.add("Emily");
+console.log(accounting.employees);
